3e28817b75b28b72b9d1e8f28f6f418b424c05c5
tmh741 <tmh741@bu.edu>
2019-11-01 15:09:15 -0400

    Cleaned more

--- HoganDBAssignment.R
+++ HoganDBAssignment.R
@@ -8,17 +8,27 @@ contrib_all <- read.csv("Direct Contributions & JFC Dist-Table 1.csv",header=T,n
 
 JFC <- read.csv("JFC Contributions (DO NOT SUM W-Table 1.csv",header=T,na.strings="NA")
 
-contributions <- select(contrib_all, fectransid, recipid, contrib, date, amount)
+contrib_all <- apply(contrib_all,2,toupper)
+contrib_all <- as.data.frame(contrib_all)
+
+contrib_all$contrib <- gsub(", ", "/", contrib_all$contrib)
+contrib_all$contrib <- sub(" .+","",contrib_all$contrib)
+contrib_all$contrib <- sub(",.+","",contrib_all$contrib)
+contrib_all$contrib <- sub("/",", ",contrib_all$contrib)
+contrib_all$contrib <- sub("/","",contrib_all$contrib)
+
+families <- select(contrib_all, contribid, fam, contrib, lastname)
+families <- families[grep(",",families$contrib),]
+families %<>% group_by(contribid,fam) %>% slice(which.min(nchar(as.character(contrib)))) %>% distinct()
+
+contributions <- select(contrib_all, fectransid, recipid, contribid, fam, date, amount)
 contributions %<>% distinct()
 
-contributors <- select(contrib_all, contrib, City, 
+contributors <- select(contrib_all, contribid, fam, City, 
                        State, Zip, Fecoccemp, orgname)
 contributors %<>% distinct()
 
-families <- select(contrib_all, contrib, contribid, fam, lastname)
-families %<>% distinct()
-
-org <- select(contrib_all, orgname, ultorg)
+org <- select(contrib_all, Fecoccemp, orgname, ultorg)
 org %<>% distinct()
 
 cycles <- select(contrib_all,date,cycle)
@@ -27,14 +37,27 @@ cycles %<>% distinct()
 recipient <- select(contrib_all, recipid, recipient, type, party, recipcode, cmteid)
 recipient %<>% distinct()
 
-assignmentdb <- dbConnect(SQLite(), "tim-db.sqlite")
-dbWriteTable(assignmentdb, "org", org)
+assignmentdb <- dbConnect(SQLite(), "tim-db.sqlite",ov)
+dbWriteTable(assignmentdb, "contributions", contributions)
 dbWriteTable(assignmentdb, "contributors",contributors)
+dbWriteTable(assignmentdb, "org", org)
 dbWriteTable(assignmentdb, "families",families)
 dbWriteTable(assignmentdb, "cycles",cycles)
-dbWriteTable(assignmentdb, "contributions", contributions)
 dbWriteTable(assignmentdb,"recipient", recipient)
 dbDisconnect(assignmentdb)
 
 
+#length(unique(JFC$ultorg))
+
+#common <- intersect(contrib_all$orgname,JFC$orgname)
+#length(common)
+#length(common) - length(unique(JFC$orgname))
+
+#common2 <- intersect(contrib_all$Fecoccemp,JFC$Fecoccemp)
+#length(common2)
+#length(common2) - length(unique(JFC$Fecoccemp))
+
+#common3 <- intersect(contrib_all$ultorg,JFC$ultorg)
+#length(common3)
+#length(common3) - length(unique(JFC$ultorg))
 
